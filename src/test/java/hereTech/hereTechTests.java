849bbb4f7716221db96e07a2c6b2eb919ba7a3ae
AD60Q3Q <aditya.b.pandey@capgemini.com>
2022-02-10 15:37:46 +0530

    added files

--- src/test/java/hereTech/hereTechTests.java
+++ src/test/java/hereTech/hereTechTests.java
@@ -1,5 +1,6 @@
 package hereTech;
 
+import jdk.jfr.Description;
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -57,66 +58,66 @@ public class hereTechTests extends Base {
     public void test1_verifyGoogleMaps(String browser) throws IOException {
         try{
             assertion = new SoftAssert();
-        if(browser.equalsIgnoreCase("Chrome")){
+            if(browser.equalsIgnoreCase("Chrome")){
 
-        //navigating to the google maps page
-            String placeName = "Wankhede Stadium";
-            String frameTitleName = "Wankhede Stadium - Google Maps";
-            String phoneNumber = "022 2279 5500";
-            driver.get("https://www.google.com/maps/");
+                //navigating to the google maps page
+                String placeName = "Wankhede Stadium";
+                String frameTitleName = "Wankhede Stadium - Google Maps";
+                String phoneNumber = "022 2279 5500";
+                driver.get("https://www.google.com/maps/");
 
-            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
-            //waiting till search box appear
-            wait.until(ExpectedConditions.visibilityOf(mapsPageObj.searchBox));
-            mapsPageObj.searchBox.sendKeys(placeName);
-            mapsPageObj.searchButton.click();
+                WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+                //waiting till search box appear
+                wait.until(ExpectedConditions.visibilityOf(mapsPageObj.searchBox));
+                mapsPageObj.searchBox.sendKeys(placeName);
+                mapsPageObj.searchButton.click();
 
-            //waiting till photo of wankhede appears
-            wait.until(ExpectedConditions.visibilityOf(mapsPageObj.photoWankhede));
-            takeScreenShot("TC-01_googleMaps_1");
+                //waiting till photo of wankhede appears
+                wait.until(ExpectedConditions.visibilityOf(mapsPageObj.photoWankhede));
+                takeScreenShot("TC-01_googleMaps_1");
 
-            //Verifying the text Stadium
-            assertion.assertTrue(mapsPageObj.stadiumText.getText().equalsIgnoreCase("Stadium"),"Stadium text is present");
+                //Verifying the text Stadium
+                assertion.assertTrue(mapsPageObj.stadiumText.getText().equalsIgnoreCase("Stadium"),"Stadium text is present");
 
-            //Verifying the title
-            System.out.println(mapsPageObj.stadiumFrameTitle.getText());
-            assertion.assertTrue(mapsPageObj.stadiumFrameTitle.getText().equalsIgnoreCase(frameTitleName),"The title should be " +frameTitleName +"but is " +mapsPageObj.stadiumFrameTitle.getText());
-
-
-            //Verifying and printing the rating and printing the number of reviews
-            assertTrue(mapsPageObj.ratingNumber.isDisplayed(),"Ratings is present");
-            assertTrue(mapsPageObj.numberOfreview.isDisplayed(),"Ratings is present");
-            logger.info("The rating of Google maps is "+mapsPageObj.ratingNumber.getText()+" and the number of reviewers are : "+mapsPageObj.numberOfreview.getText());
+                //Verifying the title
+                System.out.println(mapsPageObj.stadiumFrameTitle.getText());
+                assertion.assertTrue(mapsPageObj.stadiumFrameTitle.getText().equalsIgnoreCase(frameTitleName),"The title should be " +frameTitleName +"but is " +mapsPageObj.stadiumFrameTitle.getText());
 
 
-            //Verifying the mumbai cricket link
-            assertion.assertTrue(mapsPageObj.mumbaiCricketLink.isDisplayed(),"Mumbai cricket link is available");
+                //Verifying and printing the rating and printing the number of reviews
+                assertTrue(mapsPageObj.ratingNumber.isDisplayed(),"Ratings is present");
+                assertTrue(mapsPageObj.numberOfreview.isDisplayed(),"Ratings is present");
+                logger.info("The rating of Google maps is "+mapsPageObj.ratingNumber.getText()+" and the number of reviewers are : "+mapsPageObj.numberOfreview.getText());
 
-            //Verifying and printing the address
-            String addressText = mapsPageObj.address.getText();
-            logger.info("The address present above the link is : "+addressText);
 
-            //Verifying that the phone number is present
-            System.out.println(mapsPageObj.phoneNumberSection.getText());
-            assertion.assertTrue(mapsPageObj.phoneNumberSection.getText().equalsIgnoreCase(phoneNumber),"The phone shouled be "+phoneNumber+"but is "+mapsPageObj.phoneNumberSection.getText());
+                //Verifying the mumbai cricket link
+                assertion.assertTrue(mapsPageObj.mumbaiCricketLink.isDisplayed(),"Mumbai cricket link is available");
 
-            //Taking a screensnhot again
-            takeScreenShot("TC-01_googleMaps_2");
+                //Verifying and printing the address
+                String addressText = mapsPageObj.address.getText();
+                logger.info("The address present above the link is : "+addressText);
 
-            assertion.assertAll();
-        }
-    else
+                //Verifying that the phone number is present
+                System.out.println(mapsPageObj.phoneNumberSection.getText());
+                assertion.assertTrue(mapsPageObj.phoneNumberSection.getText().equalsIgnoreCase(phoneNumber),"The phone shouled be "+phoneNumber+"but is "+mapsPageObj.phoneNumberSection.getText());
 
-    {
-        logger.info("Shouldn't be tested on another browser");
-    }
+                //Taking a screensnhot again
+                takeScreenShot("TC-01_googleMaps_2");
+
+                assertion.assertAll();
+            }
+            else
+
+            {
+                logger.info("Shouldn't be tested on another browser");
+            }
         }
         catch(Exception e){
             e.printStackTrace();
         }
 
 
-        }
+    }
 
 
 
@@ -306,41 +307,41 @@ public class hereTechTests extends Base {
     @Test(priority = 05, description = "TC-06 : Verify Google maps")
     public void test4_verifygoogleMaps(String browser) throws IOException, InterruptedException {
         if(browser.equalsIgnoreCase("Chrome")){
-        //navigating to the webpage
-        driver.get("http://maps.google.com");
-        String address = "Borivali Railway Station";
-        String fromSearchBox = "Kashigaon,Mira road";
+            //navigating to the webpage
+            driver.get("http://maps.google.com");
+            String address = "Borivali Railway Station";
+            String fromSearchBox = "Kashigaon,Mira road";
 
-        mapsPageObj.searchBox.sendKeys(address);
-        mapsPageObj.searchBox.sendKeys(Keys.ENTER);
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOf(mapsPageObj.addressText));
+            mapsPageObj.searchBox.sendKeys(address);
+            mapsPageObj.searchBox.sendKeys(Keys.ENTER);
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+            wait.until(ExpectedConditions.visibilityOf(mapsPageObj.addressText));
 
-        String addressText = mapsPageObj.addressText.getText();
-        System.out.println(addressText);
+            String addressText = mapsPageObj.addressText.getText();
+            System.out.println(addressText);
 
-        takeScreenShot("TC-06_maps_01");
-
-
-        //Direction icon is visible
-        assertTrue(mapsPageObj.directionsIcon.isDisplayed(),"Directions icon is visible");
-        wait.until(ExpectedConditions.elementToBeClickable(mapsPageObj.directionsIcon));
-        Thread.sleep(2000);
-        mapsPageObj.directionsIcon.click();
+            takeScreenShot("TC-06_maps_01");
 
 
-        Thread.sleep(2000);
-        mapsPageObj.directionFromSearchBox.sendKeys(fromSearchBox);
-        mapsPageObj.directionFromSearchBox.sendKeys(Keys.ENTER);
+            //Direction icon is visible
+            assertTrue(mapsPageObj.directionsIcon.isDisplayed(),"Directions icon is visible");
+            wait.until(ExpectedConditions.elementToBeClickable(mapsPageObj.directionsIcon));
+            Thread.sleep(2000);
+            mapsPageObj.directionsIcon.click();
 
-        Thread.sleep(4000);
-        wait.until(ExpectedConditions.visibilityOf(mapsPageObj.firstOptionDistance.get(0)));
 
-        String firstOptionDistance = mapsPageObj.firstOptionDistance.get(0).getText();
-        String firstOptionTime = mapsPageObj.firstOptionTime.get(0).getText();
+            Thread.sleep(2000);
+            mapsPageObj.directionFromSearchBox.sendKeys(fromSearchBox);
+            mapsPageObj.directionFromSearchBox.sendKeys(Keys.ENTER);
 
-        System.out.println("The first option's distance is : "+firstOptionDistance);
-        System.out.println("The first option's time is : "+firstOptionTime);
+            Thread.sleep(4000);
+            wait.until(ExpectedConditions.visibilityOf(mapsPageObj.firstOptionDistance.get(0)));
+
+            String firstOptionDistance = mapsPageObj.firstOptionDistance.get(0).getText();
+            String firstOptionTime = mapsPageObj.firstOptionTime.get(0).getText();
+
+            System.out.println("The first option's distance is : "+firstOptionDistance);
+            System.out.println("The first option's time is : "+firstOptionTime);
 
 
         }
@@ -353,31 +354,31 @@ public class hereTechTests extends Base {
     @Test(priority = 06, description = "TC-07 : Verify Data table website")
     public void test4_verifyDataTable(String browser) throws IOException, InterruptedException {
         if(browser.equalsIgnoreCase("Chrome")){
-        //navigating to the webpage
-        driver.get("https://datatables.net/");
+            //navigating to the webpage
+            driver.get("https://datatables.net/");
 
-        Select sel = new Select(dataTablePageObj.dropdownEntry);
-        sel.selectByValue("25");
-        Thread.sleep(3000);
+            Select sel = new Select(dataTablePageObj.dropdownEntry);
+            sel.selectByValue("25");
+            Thread.sleep(3000);
 
-        //click to sort by ascending age
-        dataTablePageObj.ageColumn.click();
+            //click to sort by ascending age
+            dataTablePageObj.ageColumn.click();
 
 
 //Getting the entries and entering name , location and office
-        for(int i =0;i<dataTablePageObj.ageValue.size();i++){
-            String st = dataTablePageObj.ageValue.get(i).getText();
-            int ageNumber = Integer.parseInt(st);
+            for(int i =0;i<dataTablePageObj.ageValue.size();i++){
+                String st = dataTablePageObj.ageValue.get(i).getText();
+                int ageNumber = Integer.parseInt(st);
 
-            if(ageNumber<30){
-                System.out.println("The age is : " +st);
-                System.out.println("The name is : "+dataTablePageObj.nameValue.get(i).getText());
-                System.out.println("The position is : "+dataTablePageObj.positionValue.get(i).getText());
-                System.out.println("The office is : "+dataTablePageObj.officeValue.get(i).getText());
-                System.out.println("*************************************************");
-                System.out.println();
-            }
-        }}
+                if(ageNumber<30){
+                    System.out.println("The age is : " +st);
+                    System.out.println("The name is : "+dataTablePageObj.nameValue.get(i).getText());
+                    System.out.println("The position is : "+dataTablePageObj.positionValue.get(i).getText());
+                    System.out.println("The office is : "+dataTablePageObj.officeValue.get(i).getText());
+                    System.out.println("*************************************************");
+                    System.out.println();
+                }
+            }}
         else{
             logger.info("Shouldn't be tested on another browser");
         }
@@ -415,67 +416,68 @@ public class hereTechTests extends Base {
             Thread.sleep(5000);
 
             //Calling the write to excel method
-           writeToExcel();
+            writeToExcel();
         }
         else {
             logger.info("Shouldn't be tested on another browser");
         }
 
 
-        }
-
-
-
-        public void takeScreenShot(String pathName) throws IOException {
-            File src =( (TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(src,new File("Screenshots/"+pathName+".png"));
-        }
-
-        public void writeToExcel() throws IOException {
-    FileInputStream fis = new FileInputStream("xlFIles\\redBusData.xlsx");
-    XSSFWorkbook workbook = new XSSFWorkbook(fis);
-    for (int i = 0; i < redBusPageObj.ratings.size(); i++) {
-        String ratingText = redBusPageObj.ratings.get(i).getText();
-        double ratingNum = Double.parseDouble(ratingText);
-        if (ratingNum < 4.0) {
-            System.out.println(ratingNum);
-
-
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            Row row = sheet.createRow(i + 1);
-            Cell nameCell = row.createCell(0);
-            Cell startTimeCell = row.createCell(1);
-            Cell endTimeCell = row.createCell(2);
-            Cell ratingCell = row.createCell(3);
-            Cell moneyCell = row.createCell(4);
-            Cell fromLocationCell = row.createCell(5);
-            Cell toLocationCell = row.createCell(6);
-
-            nameCell.setCellValue(redBusPageObj.nameToursAndTravels.get(i).getText());
-            startTimeCell.setCellValue(redBusPageObj.startTime.get(i).getText());
-            endTimeCell.setCellValue(redBusPageObj.endTime.get(i).getText());
-            ratingCell.setCellValue(redBusPageObj.ratings.get(i).getText());
-            moneyCell.setCellValue(redBusPageObj.moneyForTravel.get(i).getText());
-            fromLocationCell.setCellValue(redBusPageObj.fromLocation.get(i).getText());
-            toLocationCell.setCellValue(redBusPageObj.toLocation.get(i).getText());
-
-
-        }
-
-
     }
-    File f = new File("xlFiles\\redBusData.xlsx");
-    FileOutputStream fos = new FileOutputStream(f);
-    workbook.write(fos);
-    fos.close();
-    workbook.close();
-  }
+
+
+    @Description("For taking screenshot")
+    public void takeScreenShot(String pathName) throws IOException {
+        File src =( (TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src,new File("Screenshots/"+pathName+".png"));
+    }
+
+    @Description("For writing to excel")
+    public void writeToExcel() throws IOException {
+        FileInputStream fis = new FileInputStream("xlFIles\\redBusData.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        for (int i = 0; i < redBusPageObj.ratings.size(); i++) {
+            String ratingText = redBusPageObj.ratings.get(i).getText();
+            double ratingNum = Double.parseDouble(ratingText);
+            if (ratingNum < 4.0) {
+                System.out.println(ratingNum);
+
+
+                XSSFSheet sheet = workbook.getSheetAt(0);
+                Row row = sheet.createRow(i + 1);
+                Cell nameCell = row.createCell(0);
+                Cell startTimeCell = row.createCell(1);
+                Cell endTimeCell = row.createCell(2);
+                Cell ratingCell = row.createCell(3);
+                Cell moneyCell = row.createCell(4);
+                Cell fromLocationCell = row.createCell(5);
+                Cell toLocationCell = row.createCell(6);
+
+                nameCell.setCellValue(redBusPageObj.nameToursAndTravels.get(i).getText());
+                startTimeCell.setCellValue(redBusPageObj.startTime.get(i).getText());
+                endTimeCell.setCellValue(redBusPageObj.endTime.get(i).getText());
+                ratingCell.setCellValue(redBusPageObj.ratings.get(i).getText());
+                moneyCell.setCellValue(redBusPageObj.moneyForTravel.get(i).getText());
+                fromLocationCell.setCellValue(redBusPageObj.fromLocation.get(i).getText());
+                toLocationCell.setCellValue(redBusPageObj.toLocation.get(i).getText());
+
+
+            }
+
+
+        }
+        File f = new File("xlFiles\\redBusData.xlsx");
+        FileOutputStream fos = new FileOutputStream(f);
+        workbook.write(fos);
+        fos.close();
+        workbook.close();
+    }
 
 
     @AfterTest()
-       public void teardown(){
+    public void teardown(){
         driver.quit();
-        }
+    }
 
 
 
